828da3ca403968aca5895d9feac0b54e7027a091
TatianaShapoval <questopunto@gmail.com>
2021-10-04 01:36:04 +0300

    adaptive login, product, shop added

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,10 @@ $(function(){
       $('.menu__list').toggleClass('menu__list--active');
     });
 
+    $('.shop__filter-btn').on('click', function(){
+      $('.shop__filters').slideToggle();
+    });
+
     $('.footer-top__title').on('click', function(){
       $(this).next().slideToggle();
       $(this).toggleClass('active');
@@ -50,7 +54,15 @@ $(function(){
       asNavFor: '.product-slide__thumb',
       draggable: false,
       arrows: false,
-      fade: true
+      fade: true,
+      responsive: [
+        {
+          breakpoint: 1051,
+          settings: {
+            draggable: true,
+          }
+        },
+      ]
     });
 
 
@@ -61,10 +73,13 @@ $(function(){
 
     $('.button-list').on('click', function () {
       $('.product-item').addClass('product-item--list');
+      $('.shop-content__inner').addClass('shop-content__nogrid');
     });
 
     $('.button-grid').on('click', function () {
       $('.product-item').removeClass('product-item--list');
+      $('.shop-content__inner').removeClass('shop-content__nogrid');
+
     });
 
     
